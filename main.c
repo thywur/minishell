f018124b8888e47a11a902ec6df532b1a8093673
quteriss <quentin.terisse@outlook.com>
2024-04-04 16:09:53 +0200

    Norminette main.c

--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 /*   By: quteriss <quteriss@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/18 15:10:34 by quteriss          #+#    #+#             */
-/*   Updated: 2024/04/04 15:03:18 by quteriss         ###   ########.fr       */
+/*   Updated: 2024/04/04 16:09:24 by quteriss         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,8 +16,8 @@ int	g_last_signal;
 
 int	dup_env(char ***env)
 {
-	char **new_env;
-	int i;
+	char	**new_env;
+	int		i;
 
 	new_env = malloc(sizeof(char *) * (ft_arrlen(*env) + 1));
 	if (!new_env)
@@ -35,6 +35,23 @@ int	dup_env(char ***env)
 	return (0);
 }
 
+char	*read_cmdline(t_block **blocks, char **env)
+{
+	char	*cmdline;
+
+	signal_hub(1);
+	cmdline = readline(">>> ");
+	if (!cmdline)
+	{
+		printf("exit\n");
+		if (*blocks)
+			free_blocks(blocks);
+		free_arr(env);
+		return (NULL);
+	}
+	return (cmdline);
+}
+
 t_block	*process_cmdline(char *cmdline, int *exit_status, char **env)
 {
 	t_token	*tokens;
@@ -43,7 +60,7 @@ t_block	*process_cmdline(char *cmdline, int *exit_status, char **env)
 	tokens = split_cmdline_into_tokens(cmdline);
 	if (!tokens)
 		return (NULL);
-	*exit_status = expand_tokens(&tokens, env, exit_status);
+	*exit_status = expand_tokens(&tokens, env, *exit_status);
 	if (*exit_status == 4)
 		return (free_tokens(&tokens), print_error(MALLOC_ERROR), NULL);
 	if (!check_tokens(&tokens))
@@ -57,50 +74,45 @@ t_block	*process_cmdline(char *cmdline, int *exit_status, char **env)
 	return (blocks);
 }
 
+void	execute_cmdline(t_block **blocks, char **env, int *exit_status)
+{
+	int		saved_stdin;
+	int		saved_stdout;
+
+	saved_stdin = dup(STDIN_FILENO);
+	saved_stdout = dup(STDOUT_FILENO);
+	*exit_status = cmd_handler(blocks, &env);
+	dup2(saved_stdin, STDIN_FILENO);
+	dup2(saved_stdout, STDOUT_FILENO);
+	close(saved_stdin);
+	close(saved_stdout);
+	free_blocks(blocks);
+}
+
 int	main(int argc, char **argv, char **env)
 {
 	char	*cmdline;
-	t_token	*tokens;
 	t_block	*blocks;
-	int		saved_stdin;
-	int		saved_stdout;
 	int		exit_status;
 
 	(void)argv;
-	if (argc > 1)
-		return (1);
-	if (dup_env(&env) == -1)
+	if (argc > 1 || dup_env(&env) == -1)
 		return (1);
 	g_last_signal = 0;
 	exit_status = 0;
-
+	blocks = NULL;
 	while (42)
 	{
-		signal_hub(1);
-		cmdline = readline(">>> ");
+		cmdline = read_cmdline(&blocks, env);
 		if (!cmdline)
-		{
-			printf("exit\n");
-			if (blocks)
-				free_blocks(&blocks);
-			free_arr(env);
 			return (g_last_signal);
-		}
-
 		if (ft_strlen(cmdline) == 0)
 			continue ;
 		add_history(cmdline);
 		blocks = process_cmdline(cmdline, &exit_status, env);
 		if (!blocks)
 			continue ;
-		saved_stdin = dup(STDIN_FILENO);		// trouver un autre moyen de les stocker et de les close
-		saved_stdout = dup(STDOUT_FILENO);
-		exit_status = cmd_handler(&blocks, &env);
-		dup2(saved_stdin, STDIN_FILENO);
-		dup2(saved_stdout, STDOUT_FILENO);
-		close(saved_stdin);
-		close(saved_stdout);
-		free_blocks(&blocks);
+		execute_cmdline(&blocks, env, &exit_status);
 	}
 	return (g_last_signal);
 }
