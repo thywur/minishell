4fb3e6d966b67dd7972ed0063c11e96bee7e4282
quteriss <quentin.terisse@outlook.com>
2024-04-04 15:01:38 +0200

    Reduce main

--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 /*   By: quteriss <quteriss@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/18 15:10:34 by quteriss          #+#    #+#             */
-/*   Updated: 2024/03/30 17:30:57 by quteriss         ###   ########.fr       */
+/*   Updated: 2024/04/04 15:01:19 by quteriss         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -35,6 +35,28 @@ int	dup_env(char ***env)
 	return (0);
 }
 
+t_block	*process_cmdline(char *cmdline, int *exit_status, char **env)
+{
+	t_token	*tokens;
+	t_block	*blocks;
+
+	tokens = split_cmdline_into_tokens(cmdline);
+	if (!tokens)
+		return (NULL);
+	*exit_status = expand_tokens(&tokens, env, exit_status);
+	if (*exit_status == 4)
+		return (free_tokens(&tokens), print_error(MALLOC_ERROR), NULL);
+	if (!check_tokens(&tokens))
+		return (free_tokens(&tokens), NULL);
+	blocks = join_tokens_into_blocks(&tokens);
+	if (!blocks)
+	{
+		*exit_status = 4;
+		return (print_error(MALLOC_ERROR), NULL);
+	}
+	return (blocks);
+}
+
 int	main(int argc, char **argv, char **env)
 {
 	char	*cmdline;
@@ -49,46 +71,20 @@ int	main(int argc, char **argv, char **env)
 		return (1);
 	if (dup_env(&env) == -1)
 		return (1);
-
 	g_last_signal = 0;
 	exit_status = 0;
-
 	while (42)
 	{
 		signal_hub(1);
-
 		cmdline = readline(">>> ");
 		if (!cmdline)
 			return (1);
-
 		if (ft_strlen(cmdline) == 0)	
 			continue ;
 		add_history(cmdline);
-
-		tokens = split_cmdline_into_tokens(cmdline);
-		if (!tokens)
-			continue ;
-		exit_status = expand_tokens(&tokens, env, exit_status);
-		if (exit_status == 4)
-		{
-			free_tokens(&tokens);
-			print_error(MALLOC_ERROR);
-			continue ;
-		}
-		if (!check_tokens(&tokens))
-		{
-			free_tokens(&tokens);
-			continue ;
-		}
-		blocks = join_tokens_into_blocks(&tokens);
+		blocks = process_cmdline(cmdline, &exit_status, env);
 		if (!blocks)
-		{
-			print_error(MALLOC_ERROR);
-			exit_status = 4;
 			continue ;
-		}
-		// print_blocks(&blocks);
-
 		saved_stdin = dup(STDIN_FILENO);		// trouver un autre moyen de les stocker et de les close
 		saved_stdout = dup(STDOUT_FILENO);
 		exit_status = cmd_handler(&blocks, &env);
@@ -96,7 +92,6 @@ int	main(int argc, char **argv, char **env)
 		dup2(saved_stdout, STDOUT_FILENO);
 		close(saved_stdin);
 		close(saved_stdout);
-
 		free_blocks(&blocks);
 	}
 }
